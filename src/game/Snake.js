581d1b00d9cdcb415e6806f024e26b5505bb5ebd
lich <100108350@qq.com>
2019-06-15 14:43:54 +0800

    添加按键监听

---
+++ src/game/Snake.js
@@ -0,0 +1,110 @@
+import { canvas } from "../config";
+const DIRECTION = {
+    'up':1,
+    'right':2,
+    'down':3,
+    'left':4
+};
+
+/**🐍的一节身体 */
+class SnakeBody {
+    /**
+     * 
+     * @param {number} x 
+     * @param {number} y 
+     * @param {SnakeBody} next 
+     */
+    constructor(x,y,next){
+        this.x = x || 0;
+        this.y = y || 0;
+        this.next = next || next;
+    }
+}
+
+/**🐍 */
+class Snake {
+    /**
+     * @param {Number} length 
+     * @param {Number} speed 
+     * @param {DIRECTION} direction 
+     */
+    constructor(length,speed,direction){
+        this.length = length || 1;
+        this.speed = speed || 1;
+        this.direction = direction || DIRECTION.right;
+
+        /** SnakeBody */
+        this.header = null;
+
+        this.__createBody(new SnakeBody(0,0,null));
+        this.__createBody(new SnakeBody(1,0,null));
+        this.__createBody(new SnakeBody(2,0,null));
+    }
+
+    /**
+     * @param {SnakeBody} item 
+     */
+    __createBody (item){
+        item.next =  this.header;
+        this.header = item;
+    }
+    
+    /**
+     * @param {CanvasRenderingContext2D} ctx 
+     */
+    render (ctx){
+        ctx.fillStyle = '#000';
+        let node = this.header;
+        let size = canvas.size;
+        while (node) {
+            ctx.fillRect(node.x*size,node.y*size,size,size);
+            node = node.next;
+        }
+       this.move();
+    }
+
+    /**  在render 渲染一次之后进行位置的移动 
+     *  当前节点占据上一个节点的位置 。 并且缓存当前节点 ，作为 下次循环的上一个节点
+     *  生成一个超前的节点 给头节点占位
+     * */
+    move (){
+        let prev = null;
+        let speed = this.speed;
+        switch (this.direction) {
+            case DIRECTION.up:
+                prev = new SnakeBody(this.header.x,this.header.y-speed,null);
+                break;
+            case DIRECTION.right:
+                prev = new SnakeBody(this.header.x+speed,this.header.y,null);
+                break;
+            case DIRECTION.down:
+                prev = new SnakeBody(this.header.x,this.header.y+speed,null);
+                break;
+            case DIRECTION.left:
+                prev = new SnakeBody(this.header.x-speed,this.header.y,null);
+                break;
+            default:
+                break;
+        }
+        // prev = this.header;
+        // let current = prev.next;
+
+        // prev = this.header;
+        let current = this.header;
+        while (current) {
+           
+            let temp = new SnakeBody(current.x,current.y,null);
+            current.x = prev.x;
+            current.y = prev.y;
+            
+            prev = temp ;
+            current = current.next;
+        }
+
+    }
+}
+
+export  {
+    Snake,
+    DIRECTION
+} ;
--- src/index.js
+++ src/index.js
@@ -5,24 +5,52 @@ import _ from 'lodash';
 
 /**游戏组件 */
 import Background from './game/Background';
+import {Snake,DIRECTION} from "./game/Snake";
 
 
 
 /**初始化游戏组件 */
 const background = new Background(); 
-
+const snake = new Snake(); 
+snake.speed = 1
 /**
  * 每帧的更新
  * @param {CanvasRenderingContext2D} ctx 
  */
 function update(ctx) {
     background.render(ctx);
-    ctx.fillStyle = '#000';
-    ctx.fillRect(canvas.size,canvas.size,canvas.size,canvas.size);
+    snake.render(ctx);
 }
 
 
+const keyMaps = {
+    'ArrowUp':'38',
+    'ArrowRight':'39',
+    'ArrowDown':'40',
+    'ArrowLeft':'37',
+}
 
+/**全局的按键事件
+ * @param {KeyboardEvent} event 
+ */
+function eventListener(event) {
+    switch (String(event.keyCode)) {
+        case keyMaps.ArrowUp:
+            snake.direction = DIRECTION.up;
+            break;
+        case keyMaps.ArrowRight:
+            snake.direction = DIRECTION.right;
+            break;
+        case keyMaps.ArrowDown:
+            snake.direction = DIRECTION.down;
+            break;
+        case keyMaps.ArrowLeft:
+            snake.direction = DIRECTION.left;
+            break;
+        default:
+            break;
+    }
+}
 
 
 
@@ -32,6 +60,8 @@ function init() {
     var c = document.createElement('canvas');
     c.height = canvas.height;
     c.width = canvas.width;
+    // c.addEventListener('click')
+    window.addEventListener('keydown',eventListener)
     var ctx = c.getContext("2d");
     document.body.append(c);
     function loop() {
